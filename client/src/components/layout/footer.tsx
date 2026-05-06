a03b9aec4064ab75952777cf4d75191b4ddc1a3c
Rahul-singh1998 <whoraul1998@gmail.com>
2026-05-06 19:55:12 +0530

    Optimize GTM loading to reduce TBT and improve CWV

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ import { FaInstagram, FaLinkedin, FaFacebook, FaYoutube } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="bg-darkblue text-white pt-12 pb-6">
+    <footer className="bg-darkblue text-white pt-12 pb-6 min-h-[420px]">
       <div className="container mx-auto px-4 md:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mb-8">
 
